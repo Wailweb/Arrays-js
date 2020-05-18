d1e907a6a75e7b6e5339b1a6c8f2f65834426507
Wailweb <wail.sdni@gmail.com>
2020-05-18 18:23:27 +0200

    slice

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var fruits = ["Pomme", "Banane", "Orange", "Citron"];
 
-for (var i = 0; i < fruits.length; i++) {
-  console.log(fruits[i]);
-}
+// for (var i = 0; i < fruits.length; i++) {
+//  console.log(fruits[i]);
+// }
+
+
+var agrumes = fruits.slice(2,4);
+
+console.log(agrumes);
